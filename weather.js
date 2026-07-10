ddbc3249c4293c1ad16e225f639e9443a7043813
aranchini <s26032@gsm.hs.kr>
2026-07-10 15:31:41 +0900

    nico

--- weather.js
+++ weather.js
@@ -4,14 +4,13 @@ function onGeoOK(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     console.log("너가 사는 곳은",lat,lon);
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric
-`
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url).then(Response => Response.json())
     .then(data => {
         const weather = document.querySelector("#weather span:first-child");
         const city = document.querySelector("#weather span:last-child");
         city.innerText=data.name;
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp};`
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     });
 }
 
